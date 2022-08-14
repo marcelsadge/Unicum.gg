bb8572e899e37051d8ce2c13c5bdff8491798b19
Junhao Zhang <52556235+JujuOW@users.noreply.github.com>
2022-08-14 02:34:48 -0400

    Change home page design

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
+
 import SearchBar from '../SearchBar';
+import NavBar from '../NavBar';
 import './index.css';
 
 function HomePage() {
     return(
-        <div className='container'>
-            <h1 className='header'>
-                Unicum.gg
-            </h1>
-            <div className='search-bar-container'>
-                <SearchBar
-                />
+        <div>
+            <NavBar />
+            <div className='container'>
+                <h1 style={{ color: '#fff', fontSize: 36 }}>
+                    Unicum.gg
+                </h1>
+                <div className='search-bar-container'>
+                    <SearchBar
+                    />
+                </div>
+                <br />
+                <h1 style={{ color: '#fff', fontSize: 12 }}>
+                    The Most Competitive Stats Site For World Of Tanks
+                </h1>
             </div>
         </div>
     );
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,6 +1,30 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import InputBase from '@mui/material/InputBase';
+
 import './index.css';
+
+const SearchForm = styled.form`
+    margin-left: 1rem;
+`
+const SearchBox = styled.div`
+    padding: 2px 4px;
+    display: flex;
+    align-items: center;
+    width: 700px;
+    height: 50px;
+    background-color: #242729;
+    color: white;
+    :hover {
+        background-color: #222527;
+        box-shadow: 0px 1px 3px rgba(20, 20, 50, 1) ;
+    }
+`
+
+const SearchInput = styled(InputBase)`
+    width: 100%
+`
 
 function SearchBar() {
     const [type, setType] = useState('PLYR');
@@ -23,22 +47,36 @@ function SearchBar() {
             setType('PLYR');
         }
     };
-
+    /*
     return (
-        <div className='search-row'>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    className='search-bar'
-                    placeholder={type === 'PLYR' ? 'Enter Player Name' : 'Enter Clan Name'}
-                    type='text' 
+        <div>
+            <div className='search-row'>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        className='search-bar'
+                        placeholder={type === 'PLYR' ? 'Enter Player Name' : 'Enter Clan Name'}
+                        type='text' 
+                        value={search} 
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </form>
+                <button onClick={toggleOption}>
+                    {type}
+                </button>
+            </div>
+        </div>
+    );
+    */
+    return (
+        <SearchForm onSubmit={handleSubmit}>
+            <SearchBox>
+                <SearchInput
+                    placeholder={type === 'PLYR' ? 'Enter Player Username' : 'Enter Clan Name'}
                     value={search} 
                     onChange={(e) => setSearch(e.target.value)}
                 />
-            </form>
-            <button onClick={toggleOption}>
-                {type}
-            </button>
-        </div>
+            </SearchBox>
+        </SearchForm>
     );
 }
 
